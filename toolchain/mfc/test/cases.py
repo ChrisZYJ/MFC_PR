9f830a475529643fef7dc3690fb102df21aefab0
Chris Zhang <chriszhangyanjun@gmail.com>
2025-04-05 14:23:52 -0700

    rebase upstream

--- toolchain/mfc/test/cases.py
+++ toolchain/mfc/test/cases.py
@@ -572,6 +572,16 @@ def list_cases() -> typing.List[TestCaseBuilder]:
 
             cases.append(define_case_d(stack, '', {}))
 
+            reflective_params = {'bc_x%beg': -2, 'bc_x%end': -2, 'bc_y%beg': -2, 'bc_y%end': -2}
+            if len(dimInfo[0]) == 3:
+                reflective_params.update({'bc_z%beg': -2, 'bc_z%end': -2})
+
+            if num_fluids == 1:
+                if len(dimInfo[0]) >= 2:
+                    cases.append(define_case_d(stack, 'bc=-2', reflective_params))
+                if len(dimInfo[0]) == 2:
+                    cases.append(define_case_d(stack, 'Axisymmetric', {**reflective_params, 'cyl_coord': 'T'}))
+
             stack.pop()
 
             if num_fluids == 2:
